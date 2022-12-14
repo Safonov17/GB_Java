c7a358fd7d30ff1c030204ea14e1c563b300d939
I.Safonov <safonovil17@yandex.ru>
2022-12-14 19:29:03 +0300

    Added solution of the first task

---
+++ Seminar1.java
@@ -0,0 +1,22 @@
+
+public class Seminar1 {
+    public static void main(String[] args) {
+        System.out.print("\033[H\033[J"); // очистка консоли
+        // Задача 1. Вычислить n-ое треугольного число(сумма чисел от 1 до n), 
+        // n! (произведение чисел от 1 до n)
+
+        // int value = 5;
+        // int result = value * (value + 1) / 2;
+        // System.out.println(result);
+
+        // int result = 1;
+        // for (int i = 1; i<=value; i++) {
+        //     result = result*i;
+        // }
+        // System.out.println(result);
+
+        // =============================================================
+        // Задача 2. Вывести все простые числа от 1 до 1000
+        
+    }
+}
